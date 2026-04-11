3260a805321aa681ce4458cbb3a7e7409549b6cd
Vivek Kumar <vkumar@ch.iitr.ac.in>
2026-04-11 09:47:29 +0530

    added some stats css

--- src/components/Statitics.jsx
+++ src/components/Statitics.jsx
@@ -1,37 +1,33 @@
 import React from 'react'
-import { BiSolidMessageSquareCheck, BiSolidMessageSquareDots, BiSolidMessageSquareEdit, BiSolidMessageSquareError, BiSolidMessageSquareX } from 'react-icons/bi';
+import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { BiMessageEdit } from 'react-icons/bi'
+import { FiSend } from 'react-icons/fi'
+import { TbClock, TbClockX, TbMessageDots, TbMessageOff, TbSend, TbXboxX } from 'react-icons/tb'
+import { VscSend } from 'react-icons/vsc'
 
 export const Statitics = ({list}) => {
     return (
         <div className="stats-container">
             <h2>Statitics</h2>
             <div className="stats">
-                <div className="status-applied card">
-                    <div className='stats-title'>
-                        <BiSolidMessageSquareCheck />
-                        <h4>APPLIED</h4>
-                    </div>
+                <div className="status-applied ">
+                    <TbSend className='stats-icon'/>
+                    <h4>Applied</h4>
                     <h1>{list.length}</h1>
                 </div>
-                <div className="status-interviewing card">
-                    <div className='stats-title'>
-                        <BiSolidMessageSquareDots />
-                        <h4>INTERVIEWED</h4>
-                    </div>
+                <div className="status-interviewing ">
+                    <TbMessageDots className='stats-icon'/>
+                    <h4>Interviewed</h4>
                     <h1>0</h1>
                 </div>
-                <div className="status-rejected card">
-                    <div className='stats-title'>
-                        <BiSolidMessageSquareX />
-                        <h4>REJECTED</h4>
-                    </div>
+                <div className="status-rejected ">
+                    <TbXboxX className='stats-icon'/>
+                    <h4>Rejected</h4>
                     <h1>3</h1>
                 </div>
-                <div className="status-no-response card">
-                    <div className='stats-title'>
-                        <BiSolidMessageSquareEdit />
-                        <h4>NO RESPONSE</h4>
-                    </div>
+                <div className="status-no-response ">
+                    <TbClock className='stats-icon'/>
+                    <h4>No-Response</h4>
                     <h1>3</h1>
                 </div>
             </div>
